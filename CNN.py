32438b1b9f13608eed9dba040deb741f277239e7
Sebpet <sebpet-9@student.ltu.se>
2026-03-03 16:03:18 +0100

    rms noise is added to speech

--- CNN.py
+++ CNN.py
@@ -1,7 +1,11 @@
+import random
+
 import torchaudio
+import torch
 import os
+from torchaudio.transforms import Resample
 from torchaudio.datasets import SPEECHCOMMANDS
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, random_split, Dataset
 import torch.nn as nn
 import torch.nn.functional as F
 class Net(nn.Module):
@@ -45,6 +49,8 @@ test_dataset = SPEECHCOMMANDS(
 )
 
 PATH_ESC50 = "./noise_audio"
+import os
+
 
 noise_files = []
 
@@ -69,8 +75,105 @@ print(f"Dataset size: {len(test_dataset_noise)}")
 print(f"Dataset size: {len(train_dataset)}")
 print(f"Dataset size: {len(val_dataset)}")
 print(f"Dataset size: {len(test_dataset)}")
-
+print("noise_files count:", len(noise_files))
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+
+print(f"Dataset size: {len(train_loader)}")
+
+SPEECH_LEN = 16000
+NOISE_HZ = 44100
+NOISE_FULL = 220500
+
+#add noise to the speech with rms level 0, 10 or 50:
+def add_rms_noise(speech, noise_level):
+    if noise_level == 0:
+        if speech.shape[1] < SPEECH_LEN:
+            speech = F.pad(speech, (0, SPEECH_LEN - speech.shape[1]))
+        speech_rms = torch.sqrt(torch.mean(speech**2))
+        noise_rms = torch.zeros(speech_rms.shape)
+        return speech , speech_rms, noise_rms
+    #pick a random noise file from the noise dataset, load it, and resample it to 16kHz
+    random_noise = random.choice(noise_files)
+    noise , sr = torchaudio.load(random_noise)
+    resample = Resample(orig_freq=sr, new_freq=16000)
+    noise_16k = resample(noise)
+    if speech.shape[1] < SPEECH_LEN:
+        speech = F.pad(speech, (0, SPEECH_LEN - speech.shape[1]))
+
+    #Shorten the noise to match the speech length if it's longer, or pad it if it's shorter
+    if noise_16k.shape[1] >= speech.shape[1]:
+        start = random.randint(0, noise_16k.shape[1] - speech.shape[1])
+        noise_16k_1s = noise_16k[:, start:start+speech.shape[1]]
+    else:
+        noise_16k_1s = F.pad(noise_16k, (0, speech.shape[1] - noise_16k.shape[1]))
+    
+    #adding noise to the speech with the specified noise level
+    speech_rms = torch.sqrt(torch.mean(speech**2))
+    noise_rms = torch.sqrt(torch.mean(noise_16k_1s**2))
+    
+    scale = (noise_level * speech_rms) / (noise_rms + 1e-10)
+    scaled_noise = noise_16k_1s * scale
+    noisy_speech = speech + scaled_noise
+    return noisy_speech, speech_rms, noise_rms
+
+all_labels = []
+
+
+#get all labels and create a list of unique labels, then assign an id to each label based on its index in the list of unique labels
+for i in range(len(train_dataset)):
+    all_labels.append(train_dataset[i][2])
+
+labels = sorted(set(all_labels))
+
+print("one label", all_labels[0],"length" ,len(all_labels))
+class NoisyDataset(Dataset):
+    def __init__(self, dataset, noise_level):
+        self.dataset = dataset
+        self.noise_level = noise_level
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        speech,sr, label, _, _  = self.dataset[idx]
+        noisy_speech, _, _ = add_rms_noise(speech, self.noise_level)
+        label_id = 0 
+        for i, label_name in enumerate(labels):
+            if label_name == label:
+                label_id = i
+                break
+        return noisy_speech, label_id
+    
+
+train_noise_0 = NoisyDataset(train_dataset, noise_level=0.0)
+"""train_noise_10 = NoisyDataset(train_dataset, noise_level=0.1)
+train_noise_50 = NoisyDataset(train_dataset, noise_level=0.5)
+
+val_noise_0 = NoisyDataset(val_dataset, noise_level=0.0)
+val_noise_10 = NoisyDataset(val_dataset, noise_level=0.1)
+val_noise_50 = NoisyDataset(val_dataset, noise_level=0.5)
+
+test_noise_0 = NoisyDataset(test_dataset, noise_level=0.0)
+test_noise_10 = NoisyDataset(test_dataset, noise_level=0.1)
+test_noise_50 = NoisyDataset(test_dataset, noise_level=0.5)
+"""
+
+
+train_noise_loader_0 = DataLoader(train_noise_0, batch_size=1, shuffle=True) #speech and labels with noise level 0.0
+""" 
+
+train_noisy_loader_10 = DataLoader(train_noisy_10, batch_size=1, shuffle=True)
+train_noisy_loader_50 = DataLoader(train_noisy_50, batch_size=1, shuffle=True)
+
+val_noisy_loader_0 = DataLoader(val_noisy_0, batch_size=1, shuffle=False)
+val_noisy_loader_10 = DataLoader(val_noisy_10, batch_size=1, shuffle=False)
+val_noisy_loader_50 = DataLoader(val_noisy_50, batch_size=1, shuffle=False)
+
+test_noisy_loader_0 = DataLoader(test_noisy_0, batch_size=1, shuffle=False)
+test_noisy_loader_10 = DataLoader(test_noisy_10, batch_size=1, shuffle=False)
+test_noisy_loader_50 = DataLoader(test_noisy_50, batch_size=1, shuffle=False)
+ """
